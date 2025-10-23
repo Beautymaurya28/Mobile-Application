c66a6e7bebdbe061222d52d0b0124499708c3b15
Nova <beautymaurya9142@gmail.com>
2025-10-23 14:10:54 +0530

    Add files via upload

---
+++ pract1/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pract1',
+  webDir: 'www'
+};
+
+export default config;
